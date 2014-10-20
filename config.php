952589af9fe52c9cb20f86f5e0e3a0b763dff9c3
anttisirkiahome <antti.sirkia@futurice.com>
2014-10-21 01:15:33 +0300

    hyps

--- config.php
+++ config.php
@@ -1,16 +1,18 @@
 <?php
 
-define ('RACES', 	serialize ( array ( '1', '2', '3', '4')));
-define ('RANKS', 	serialize ( (range( 1, 13 ))));
+	define ('RACES', 	serialize ( array ( '1', '2', '3', '4')));
+	define ('RANKS', 	serialize ( (range( 1, 13 ))));
 
-define ('WINNINGS', serialize ( array(
-	'Royal flush' 		=> 5000,
-	'Straight flush' 	=> 2000,
-	'Four of a kind' 	=> 200,
-	'Full house' 		=> 50,
-	'Flush' 			=> 20,
-	'Straight' 			=> 10,
-	'Three of a kind' 	=> 5,
-	'Two pair' 			=> 2,
-	'One pair' 			=> 1
-)));
+	define ('WINNINGS', serialize ( array(
+		'Royal flush' 		=> 5000,
+		'Straight flush' 	=> 2000,
+		'Four of a kind' 	=> 200,
+		'Full house' 		=> 50,
+		'Flush' 			=> 20,
+		'Straight' 			=> 10,
+		'Three of a kind' 	=> 5,
+		'Two pair' 			=> 2,
+		'One pair' 			=> 1
+	))
+
+);
